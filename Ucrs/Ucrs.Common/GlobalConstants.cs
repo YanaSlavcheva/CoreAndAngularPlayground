871946a7f99fbe8cb7682d8d9c43c9b9e908b605
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-12-01 00:28:41 +0200

    Added Ucrs.Common/GlobalConstants.cs

---
+++ Ucrs/Ucrs.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Ucrs.Common
+{
+    public static class GlobalConstants
+    {
+        public const int EmailMaxLength = 80;
+        public const int EmailMinLength = 6;
+        public const string EmailRegEx = "^[A-Za-z0-9]+[\\._A-Za-z0-9-]+@([A-Za-z0-9]+[-\\.]?[A-Za-z0-9]+)+(\\.[A-Za-z0-9]+[-\\.]?[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    }
+}
